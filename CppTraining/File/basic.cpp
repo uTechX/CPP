a11ae9974d650538b15914eaa1f65f947d42ebdf
Jay Ha <jay@Jays-iMac-718.local>
2020-12-04 16:22:08 -0800

    Edit변경

--- CppTraining/File/basic.cpp
+++ CppTraining/File/basic.cpp
@@ -5,55 +5,55 @@ using namespace std;
 
 int main(void)
 {
-    //fstream fin("test.txt")
-    fstream fin;
-    fin.open("test.txt");
+  //fstream fin("test.txt")
+  fstream fin;
+  fin.open("test.txt");
 
-if (!fin.is_open())
-{
-    cout << "File open Error." << endl;
-    exit(0); 
+  if (!fin.is_open())
+  {
+      cout << "File open Error." << endl;
+      exit(0); 
 
-}
-cout << " File open OK \n";
+  }
+  cout << " File open OK \n";
 
 /*
 // 파일에서 문자 하나씩 읽기
-char filechar;
-while (true) 
-{
-  fin.get(filechar);
-  if (fin.fail()) 
+  char filechar;
+  while (true) 
   {
-    break;
+    fin.get(filechar);
+    if (fin.fail()) 
+    {
+      break;
+    }
+    cout << filechar << endl;
   }
-  cout << filechar << endl;
-}
 
 */
 
 
-// 파일에서 문자 한 단어씩 읽기
-char filestring[20];
+  // 파일에서 문자 한 단어씩 읽기
+  char filestring[20];
 
-while (!fin.eof())
-{
-  fin >> filestring;
-  cout << filestring << endl; 
-}
+  while (!fin.eof())
+  {
+    fin >> filestring;
+    cout << filestring << endl; 
+  }
 
 
-/*
-// 파일에서 문자 한 줄씩 읽기
-string fileline;
-while (!fin.eof())
-{
-  getline(fin, fileline);
-  cout << fileline << endl; 
-}
-*/
+  /*
+  // 파일에서 문자 한 줄씩 읽기
+  string fileline;
+  while (!fin.eof())
+  {
+    getline(fin, fileline);
+    cout << fileline << endl; 
+  }
+  */
 
-fin.close();
-//getchar();
+  fin.close();
+  //getchar();
 
 }
